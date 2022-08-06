7261ad130c9126ebdc01d2a15741ddc927e072b0
landris006 <laczko.andris007@gmail.com>
2022-08-06 09:35:37 +0200

    basic modal

--- components/MenuItem.tsx
+++ components/MenuItem.tsx
@@ -1,26 +1,41 @@
 import Image, { StaticImageData } from 'next/image';
+import { useState } from 'react';
 import styles from '../styles/MenuItem.module.scss';
+import Modal from './Modal';
 
 interface MenuProps {
   title: string;
   imageUrl: string | StaticImageData;
 }
 const MenuItem = ({ title, imageUrl }: MenuProps) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
-    <div className={styles.menuItem}>
-      <h3 className={styles.foodTitle}>{title}</h3>
-      <div className={styles.imageWrapper}>
-        <div className={styles.frame}>
-          <Image
-            className={styles.foodImage}
-            src={imageUrl}
-            width={272}
-            height={272}
-            alt="soup"
-          />
+    <>
+      <Modal
+        classNames={styles.courseInfoModal}
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+      >
+        asd
+      </Modal>
+
+      <div className={styles.menuItem}>
+        <h3 className={styles.foodTitle}>{title}</h3>
+        <div className={styles.imageWrapper}>
+          <div className={styles.frame}>
+            <Image
+              className={styles.foodImage}
+              src={imageUrl}
+              width={272}
+              height={272}
+              onClick={() => setIsModalOpen(true)}
+              alt="soup"
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
---
+++ components/Modal.tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from 'react';
+import { createPortal } from 'react-dom';
+
+interface ModalProps {
+  children: React.ReactNode;
+  isOpen: boolean;
+  onClose: () => void;
+  classNames?: string;
+}
+
+const Modal = ({ children, isOpen, onClose, classNames }: ModalProps) => {
+  console.log(classNames);
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if (e.key === 'Escape') {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    document.body.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.body.removeEventListener('keydown', handleKeyDown);
+    };
+  });
+
+  if (!isOpen) {
+    return <></>;
+  }
+
+  return createPortal(
+    <div
+      className="modalWrapper"
+      onClick={(e) => {
+        if (e.target === document.querySelector('.modalWrapper')) {
+          onClose();
+        }
+      }}
+    >
+      <div className={`modal ${classNames}`}>{children}</div>
+    </div>,
+    document.getElementById('__next')!
+  );
+};
+
+export default Modal;
